76591940fe5a313d16942ab7fb99dacea4107b38
Wu Di <wudiac@gmail.com>
2013-06-13 23:21:59 +0900

    Init commit

---
+++ lol/LCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  LCAppDelegate.h
+//  lol
+//
+//  Created by Di Wu on 6/13/13.
+//  Copyright (c) 2013 WUDI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ lol/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  lol
+//
+//  Created by Di Wu on 6/13/13.
+//  Copyright (c) 2013 WUDI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "LCAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([LCAppDelegate class]));
+  }
+}
